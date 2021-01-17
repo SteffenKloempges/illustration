b8b0322fcad5331656f40e46950f39dd334dd290
SteffenKloempges <steffen.kloempges@gmail.com>
2021-01-17 17:04:46 +0100

    addJSaddHover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 function darkMode() {
     let element = document.body
     element.classList.toggle("dark-mode")
+    document.getElementById('Img1').src = 'assets/img/darkmode-img4.png'
+    document.getElementById('Img2').src = 'assets/img/darkmode-img5.png'
+    document.getElementById('Img3').src = 'assets/img/darkmode-img6.png'
 }
